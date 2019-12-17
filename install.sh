0ea54a4808eed5dca51603a80702424c02629f73
Lukas Normantas <lukas.normantas@worldfirst.com>
2019-12-17 08:30:23 +0000

    🔧 Add VSCode user configuration

--- install.sh
+++ install.sh
@@ -11,6 +11,7 @@ cd ~
 ln --backup -s $SCRIPT_DIR/.bashrc ~/.bashrc
 ln --backup -s $SCRIPT_DIR/.profile ~/.profile
 ln --backup -s $SCRIPT_DIR/.tmux.conf ~/.tmux.conf
+ln --backup -s $SCRIPT_DIR/.config/Code/User/settings.json ~/.config/Code/User/settings.json
 
 mkdir ~/bin -p
 ln --backup -sn $SCRIPT_DIR/bin/scripts ~/bin/scripts
